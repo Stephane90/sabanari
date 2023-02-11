b26fc758bbb7015adeed740de55d400973c879fb
thaos <thao.stephane@gmail.com>
2023-02-11 23:47:21 +0100

    bugfix: booklist pagination

--- Books/booksList.js
+++ Books/booksList.js
@@ -1,3 +1,7 @@
+var pageCount = 0
+var currentPage = 0
+var paginationLimit = 10
+
 function searchBooks(event, pageCount, currentPage, paginationLimit){
     event.preventDefault()
     console.log(pageCount)
@@ -6,7 +10,7 @@ function searchBooks(event, pageCount, currentPage, paginationLimit){
     var input = document.getElementById('search').value;
     console.log("test")
     let books = []
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${input}&max-results=${paginationLimit}&start-index=${currentPage}`)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${input}&maxResults=${paginationLimit}&startIndex=${currentPage}`)
         .then(response => response.json())
         .then(data => { data.items.map((item) => {
             const volume = item.volumeInfo
@@ -22,11 +26,12 @@ function searchBooks(event, pageCount, currentPage, paginationLimit){
             }
             books.push(book)
             document.getElementById("display-books").innerHTML += "<div> title : "+book.title+
-                "</div><div> authors :"+book.authors+
-                "</div><div> language: "+book.language+
-                "</div><div> number of pages :"+book.pages+
-                "</div><div> description :"+book.description+
-                "</div><img src=\""+book.images.smallThumbnail+"\"/><br/><br/>"
+            "</div><div> authors :"+book.authors+
+            "</div><div> language: "+book.language+
+            "</div><div> number of pages :"+book.pages+
+            "</div><div> description :"+book.description+
+            (book.images && book.images.smallThumbnail ? "<img src=\""+book.images.smallThumbnail+"\"/>" : "") +
+            "<br/><br/>"
             return book
         })
             
@@ -43,3 +48,9 @@ function searchBooks(event, pageCount, currentPage, paginationLimit){
 
     
 }
+
+function next() {
+    currentPage += paginationLimit
+    searchBooks(event, pageCount, currentPage, paginationLimit)
+}
+
